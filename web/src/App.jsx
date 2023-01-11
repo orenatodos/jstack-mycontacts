f74a84ef735082932b7b232a7ad831eed8e3c85f
Renato Silva <orenatodos@gmail.com>
2023-01-11 19:44:21 -0300

    feat(web): create global styles

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyles } from './assets/styles/global'
+import { defaultTheme } from './assets/styles/themes'
+
 export function App () {
-  return <h1>MyContacts</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>MyContacts</h1>
+      <GlobalStyles />
+    </ThemeProvider>
+  )
 }
---
+++ web/src/assets/styles/global.jsx
@@ -0,0 +1,28 @@
+import { createGlobalStyle, css } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  ${({ theme }) => css`
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    body {
+      background: ${theme.backgroundColor};
+    }
+
+    body,
+    button,
+    input,
+    select,
+    textarea {
+      font-family: 'Sora', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      font-size: 16px;
+    }
+
+    button {
+      cursor: pointer;
+    }
+  `}
+`
