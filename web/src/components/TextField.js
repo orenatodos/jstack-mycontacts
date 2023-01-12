1c0658c17dacc83dc7cc6ba9e009bce1075e328c
Renato Silva <orenatodos@gmail.com>
2023-01-11 21:50:01 -0300

    feat(web): create TextField component

---
+++ web/src/components/TextField.js
@@ -0,0 +1,19 @@
+import styled, { css } from 'styled-components'
+
+export const TextField = styled.input`
+  ${({ theme }) => css`
+    width: 100%;
+    background: #FFF;
+    border: 2px solid transparent;
+    border-radius: 4px;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+    height: 52px;
+    outline: 0;
+    padding: 0 16px;
+    transition: border-color 0.2s ease-in;
+
+    &:focus {
+      border-color: ${theme.colors.primary.main};
+    }
+  `}
+`
