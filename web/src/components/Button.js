4982ee774d55cb6e7724efe249d3e9ac9662d407
Renato Silva <orenatodos@gmail.com>
2023-01-11 21:49:47 -0300

    feat(web): create Button component

---
+++ web/src/components/Button.js
@@ -0,0 +1,32 @@
+import styled, { css } from 'styled-components'
+
+export const Button = styled.button`
+  ${({ theme }) => css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    background: ${theme.colors.primary.main};
+    border: 0;
+    border-radius: 4px;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+    color: #FFF;
+    font-weight: bold;
+    height: 52px;
+    outline: 0;
+    padding: 0 16px;
+    transition: background 0.2s ease-in;
+
+    &:enabled:hover {
+      background: ${theme.colors.primary.light};
+    }
+
+    &:enabled:active {
+      background: ${theme.colors.primary.dark};
+    }
+
+    &:disabled {
+      background: #CCC;
+      cursor: default;
+    }
+  `}
+`
