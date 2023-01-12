4a13780452763b56eea53a4af0534114ea1d1421
Renato Silva <orenatodos@gmail.com>
2023-01-12 11:31:01 -0300

    refactor(web): replace homepage anchors to react router dom link

--- web/src/pages/Home/Home.jsx
+++ web/src/pages/Home/Home.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import arrowIcon from '../../assets/icons/arrow.svg'
 import editIcon from '../../assets/icons/edit.svg'
 import trashIcon from '../../assets/icons/trash.svg'
@@ -14,7 +16,7 @@ export function Home () {
       <Styled.Header>
         <strong>3 contatos</strong>
 
-        <a href="#">Novo contato</a>
+        <Link to="/new">Novo contato</Link>
       </Styled.Header>
 
       <Styled.ListContainer>
@@ -38,31 +40,9 @@ export function Home () {
           </div>
 
           <div className="actions">
-            <a href="#">
+            <Link to="/edit/123">
               <img src={editIcon} alt="Edit" width={20} />
-            </a>
-
-            <button type="button">
-              <img src={trashIcon} alt="Delete" width={20} />
-            </button>
-          </div>
-        </Styled.Card>
-
-        <Styled.Card>
-          <div className='info'>
-            <div className="contact-name">
-              <strong>Renato Silva</strong>
-              <small>Instagram</small>
-            </div>
-
-            <span>orenatodos@gmail.com</span>
-            <span>(11) 99999-9999</span>
-          </div>
-
-          <div className="actions">
-            <a href="#">
-              <img src={editIcon} alt="Edit" width={20} />
-            </a>
+            </Link>
 
             <button type="button">
               <img src={trashIcon} alt="Delete" width={20} />
