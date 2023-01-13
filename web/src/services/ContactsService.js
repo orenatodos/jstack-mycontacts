fb660dacee7f257871294b0eab5d56e931964057
Renato Silva <orenatodos@gmail.com>
2023-01-13 15:48:54 -0300

    feat(web): create contacts service

---
+++ web/src/services/ContactsService.js
@@ -0,0 +1,18 @@
+
+import { HttpClient } from './utils/HttpClient'
+
+class ContactsService {
+  constructor () {
+    this.httpClient = new HttpClient('http://localhost:3001')
+  }
+
+  async listContacts (orderBy = 'ASC') {
+    return this.httpClient.get(`/contacts?orderBy=${orderBy}`)
+  }
+
+  async createContact (contact) {
+    return this.httpClient.get('/contacts', contact)
+  }
+}
+
+export default new ContactsService()
