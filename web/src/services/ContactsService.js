ca0a29a2a6a04f9ab631236d327af49d2468acab
Renato Silva <orenatodos@gmail.com>
2023-01-15 15:38:58 -0300

    fix(web): create contact

--- web/src/services/ContactsService.js
+++ web/src/services/ContactsService.js
@@ -11,7 +11,9 @@ class ContactsService {
   }
 
   async createContact (contact) {
-    return this.httpClient.post('/contacts', contact)
+    return this.httpClient.post('/contacts', {
+      body: contact
+    })
   }
 }
 
