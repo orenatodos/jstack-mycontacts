3478ca1d07c0362373acb11da697dce71fb4b58e
Renato Silva <orenatodos@gmail.com>
2023-01-16 18:33:49 -0300

    feat(web): implement create new contact and edit contact page

--- web/src/pages/EditContact/EditContact.jsx
+++ web/src/pages/EditContact/EditContact.jsx
@@ -1,5 +1,96 @@
+import { useEffect, useRef, useState } from 'react'
+import { useHistory, useParams } from 'react-router-dom'
+
+import ContactsService from '../../services/ContactsService'
+
+import { toast } from '../../utils/toast'
+
+import { ContactForm } from '../../components/ContactForm'
+import { Loader } from '../../components/Loader'
 import { PageHeader } from '../../components/PageHeader'
 
 export function EditContact () {
-  return <PageHeader title="Editar Renato Silva" />
+  const [isLoading, setIsLoading] = useState(true)
+  const [contactName, setContactName] = useState('')
+
+  const contactFormRef = useRef(null)
+
+  const { id } = useParams()
+  const history = useHistory()
+
+  useEffect(() => {
+    async function loadContact () {
+      try {
+        const contact = await ContactsService.getContactById(id)
+
+        setContactName(contact.name)
+
+        contactFormRef.current.setFieldsValue(contact)
+
+        setIsLoading(false)
+      } catch {
+        history.push('/')
+
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!'
+        })
+      }
+    }
+
+    loadContact()
+  }, [id, history])
+
+  async function handleSubmit ({
+    name,
+    email,
+    phone,
+    categoryId
+  }) {
+    try {
+      const contact = {
+        name,
+        email,
+        phone,
+        category_id: categoryId
+      }
+
+      const updatedContact = await ContactsService.updateContact(
+        id,
+        contact
+      )
+
+      setContactName(updatedContact.name)
+
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso!'
+      })
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar o contato!'
+      })
+    }
+  }
+
+  return (
+    <>
+      <Loader isLoading={isLoading} />
+
+      <PageHeader
+        title={
+          isLoading
+            ? 'Carregando...'
+            : `Editar ${contactName}`
+        }
+      />
+
+      <ContactForm
+        ref={contactFormRef}
+        buttonLabel='Salvar alterações'
+        onSubmit={handleSubmit}
+      />
+    </>
+  )
 }
--- web/src/pages/NewContact/NewContact.jsx
+++ web/src/pages/NewContact/NewContact.jsx
@@ -1,5 +1,54 @@
+import { useRef } from 'react'
+
+import ContactsService from '../../services/ContactsService'
+
+import { toast } from '../../utils/toast'
+
+import { ContactForm } from '../../components/ContactForm'
 import { PageHeader } from '../../components/PageHeader'
 
 export function NewContact () {
-  return <PageHeader title="Novo contato" />
+  const contactFormRef = useRef(null)
+
+  async function handleSubmit ({
+    name,
+    email,
+    phone,
+    categoryId
+  }) {
+    try {
+      const contact = {
+        name,
+        email,
+        phone,
+        category_id: categoryId
+      }
+
+      await ContactsService.createContact(contact)
+
+      contactFormRef.current.resetFields()
+
+      toast({
+        type: 'success',
+        text: 'Contato cadastrado com sucesso!'
+      })
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao cadastrar o contato!'
+      })
+    }
+  }
+
+  return (
+    <>
+      <PageHeader title="Novo contato" />
+
+      <ContactForm
+        ref={contactFormRef}
+        buttonLabel='Cadastrar'
+        onSubmit={handleSubmit}
+      />
+    </>
+  )
 }
