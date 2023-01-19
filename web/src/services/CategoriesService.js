517467400310cb60713d77492b43ca042b537a4d
Renato Silva <orenatodos@gmail.com>
2023-01-19 11:54:54 -0300

    feat(web): create category mapper

--- web/src/services/CategoriesService.js
+++ web/src/services/CategoriesService.js
@@ -1,13 +1,17 @@
 
 import { HttpClient } from './utils/HttpClient'
 
+import CategoryMapper from './mappers/CategoryMapper'
+
 class CategoriesService {
   constructor () {
     this.httpClient = new HttpClient('http://localhost:3001')
   }
 
   async listCategories (orderBy = 'ASC') {
-    return this.httpClient.get(`/categories?orderBy=${orderBy}`)
+    const categories = await this.httpClient.get(`/categories?orderBy=${orderBy}`)
+
+    return categories.map(CategoryMapper.toDomain)
   }
 }
 
